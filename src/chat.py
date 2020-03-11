8efbf15cf7ff4810757aee3d5ef0a9f62f3f718f
angeliquetaylor <amt062@eng.ucsd.edu>
2020-03-11 17:39:51 -0400

    chatting

---
+++ src/chat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from sound_play.libsoundplay import SoundClient
+
+
+soundhandle = SoundClient()
+
+def callback(data):
+    rospy.sleep(1)
+    soundhandle.say("I heard " + data.data)
+    rospy.sleep(2)
+
+
+if __name__ == '__main__':
+    rospy.init_node('talker', anonymous=True)
+
+
+    rospy.Subscriber("/recognizer/output", String, callback)
+
+
+    rospy.spin()
